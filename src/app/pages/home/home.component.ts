a10763c47cb463281c9f401e309fe557588500cf
KonectaPe <dtjmestas@gmail.com>
2024-12-18 17:53:18 -0500

    agregando redes sociales

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { SocialComponent } from "../../components/social/social.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, SocialComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
