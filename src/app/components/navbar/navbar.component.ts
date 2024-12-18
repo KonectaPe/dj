29f505b8e09a15281977bb1a98a354abf91b67f5
KonectaPe <dtjmestas@gmail.com>
2024-12-17 23:10:45 -0500

    first commit

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {
+  isOpen: boolean = false;
+  toggleMenu(): void {
+    this.isOpen = !this.isOpen;
+    if (this.isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+  }
+}
