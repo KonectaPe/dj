2f80f24348900869d56151b2f1082e98371e9df7
KonectaPe <dtjmestas@gmail.com>
2024-12-19 22:30:34 -0500

    agregando sección de educación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { HomeComponent } from "./pages/home/home.component";
 import { AboutComponent } from "./pages/about/about.component";
+import { EducationComponent } from "./pages/education/education.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HomeComponent, AboutComponent],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, AboutComponent, EducationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
